1b6d8b4477bda438da9d3ca71f5e9467956bee8c
luis.gonzalez <luis.gonzalezar@sansano.usm.cl>
2022-09-11 21:41:31 -0400

    agregar indices de la pila

--- T1/main.py
+++ T1/main.py
@@ -7,6 +7,13 @@ def prepareLog():
     file.write("datetime;operacion_menu;entrada_a;entrada_ b;valor_obtenido;cod_error;desc_error\n")
     file.close()
 
+def endLog():
+    filename = os.path.join('logs', "log.txt")
+    now = datetime.now()
+    dt = now.strftime("%Y-%m-%d %H-%M")
+    newFilename = os.path.join('logs', "log "+dt+".txt")
+    os.rename(filename, newFilename)
+
 def captureInputs():
     stack = []
     for filename in os.listdir('texto'):
@@ -36,11 +43,14 @@ def elegirElemento(stack):
         if inp >= len(stack) or inp < 0:
             writeLog(1,inp_a=str(inp),err=1,err_menu2='out of bounds')
             elegirElemento(stack)
-        print("\n---Texto del elemento seleccionado de la pila [largo: {}]---\n".format(str(len(stack[inp]))))
-        print(stack[inp])
-        writeLog(1,inp_a=str(inp),value=stack[inp])
-        print('\n------------------------------------------------\n')
-    except:
+        else:
+            print("\n---Texto del elemento seleccionado de la pila [largo: {}]---\n".format(str(len(stack[inp]))))
+            print(stack[inp])
+            writeLog(1,inp_a=str(inp),value=stack[inp])
+            print('\n------------------------------------------------\n')
+            return
+    except Exception as e:
+        print(e)
         print("\nADVERTENCIA: Introduzca un número de opción válido\n")
         writeLog(1,inp_a=str(string),err=1,err_menu2='invalid input')
         elegirElemento(stack)
@@ -54,34 +64,37 @@ def comparar(stack):
         if inp >= len(stack) or inp < 0 or inp2 >= len(stack) or inp2 < 0:
             writeLog(4,inp_a=str(inp),inp_b=str(inp2),err=1,err_menu2='out of bounds')
             comparar(stack)
-        print("\n---Texto del elemento 1 [largo: {}]---\n".format(str(len(stack[inp]))))
-        print(stack[inp])
-        print('\n------------------------------------------------\n')
-        print("\n---Texto del elemento 2 [largo: {}]---\n".format(str(len(stack[inp2]))))
-        print(stack[inp2])
-        print('\n------------------------------------------------\n')
-        diff = abs(len(stack[inp])-len(stack[inp2]))
-        if diff == 0:
-            print("Los elementos 1 y 2 tienen la misma cantidad de caracteres: {}".format(str(len(stack[inp]))))
         else:
-            print("La diferencia en cantidad de caracteres del elemento 1 y 2 es: {}".format(str(diff)))
-        writeLog(4,inp_a=str(inp),inp_b=str(inp2),value=diff)
-        print('\n------------------------------------------------\n')
-    except:
+            print("\n---Texto del elemento 1 [largo: {}]---\n".format(str(len(stack[inp]))))
+            print(stack[inp])
+            print('\n------------------------------------------------\n')
+            print("\n---Texto del elemento 2 [largo: {}]---\n".format(str(len(stack[inp2]))))
+            print(stack[inp2])
+            print('\n------------------------------------------------\n')
+            diff = abs(len(stack[inp])-len(stack[inp2]))
+            if diff == 0:
+                print("Los elementos 1 y 2 tienen la misma cantidad de caracteres: {}".format(str(len(stack[inp]))))
+            else:
+                print("La diferencia en cantidad de caracteres del elemento 1 y 2 es: {}".format(str(diff)))
+            writeLog(4,inp_a=str(inp),inp_b=str(inp2),value=diff)
+            print('\n------------------------------------------------\n')
+            return
+    except Exception as e:
+        print(e)
         print("\nADVERTENCIA: Introduzca un número de opción válido\n")
         writeLog(4,inp_a=str(string),err=1,err_menu2='invalid input')
         comparar(stack)
 
 def masLargo(stack):
     lar = max(stack, key=len)
-    print("\n---Texto del elemento más largo de la pila [largo: {}]---\n".format(str(len(lar))))
+    print("\n---Texto del elemento más largo de la pila [índice: {}, largo: {}]---\n".format(str(stack.index(lar)),str(len(lar))))
     print(lar)
     writeLog(2,value=lar)
     print('\n---------------------------------------------\n')
 
 def masCorto(stack):
     shor = min(stack, key=len)
-    print("---Texto del elemento más corto de la pila [largo: {}]---\n".format(str(len(shor))))
+    print("---Texto del elemento más corto de la pila [índice: {}, largo: {}]---\n".format(str(stack.index(shor)),str(len(shor))))
     print(shor)
     writeLog(3,value=shor)
     print('\n---------------------------------------------\n')
@@ -98,7 +111,7 @@ def mainActivity(stack):
             if inp <= 0 or inp >= 6:
                 print("\nADVERTENCIA: Introduzca un número de opción válido\n")
                 writeLog(0,inp_a=inp,err=1,err_menu2='out of bounds')
-            if inp == 1:
+            elif inp == 1:
                 elegirElemento(stack)
             elif inp == 2:
                 masLargo(stack)
@@ -106,15 +119,16 @@ def mainActivity(stack):
                 masCorto(stack)
             elif inp == 4:
                 comparar(stack)
-        except:
+        except Exception as e:
+            print(e)
             print("\nIntroduzca un número de opción válido")
             writeLog(0,inp_a=string,err=1,err_menu2='invalid input')
-    quit()
 
 def main():
     stack = captureInputs()
     prepareLog()
     mainActivity(stack)
+    endLog()
 
 if __name__ == "__main__":
     main()
